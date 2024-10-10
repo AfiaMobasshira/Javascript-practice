d84caa6af392f1a50b6517937a4efb41ef225f20
AfiaMobasshira <arthyafiya93@gmail.com>
2024-10-11 03:58:41 +0600

    random number

---
+++ diceNumber.js
@@ -0,0 +1,23 @@
+// math function ceil
+let num = 2.5563001;
+let result = Math.ceil(num);
+// math function floor
+let result2 = Math.floor(num);
+// math function round
+let result3 = Math.round(num);
+
+// math function random 
+let randomNumber = Math.random() * 6;
+let diceNumber = Math.round(randomNumber);
+// console.log(diceNumber);
+
+// random number with for loop
+for(let i = 0; i <= 6; i++){
+    let randomNumb = Math.random() * 6;
+    let diceNumb = Math.round(randomNumb);
+    console.log(diceNumb);
+}
+
+
+
+
